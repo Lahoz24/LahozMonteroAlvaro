ea46d2e2fe3ec18c6187bc182c3c9ea579a64be0
Alvaro Lahoz Montero <alvarolahozmontero24@gmail.com>
2023-03-01 09:28:24 +0100

    conflicto 2

--- hyperPar.java
+++ hyperPar.java
@@ -2,7 +2,7 @@ public static boolean hyperPar(int num) {
     int resto;
     if(num=>0){
         while(num!=0){
-            resto=num%5;
+            num=resto%5;
             num=num/10;
             if(resto%2 !=0){
             return false;
